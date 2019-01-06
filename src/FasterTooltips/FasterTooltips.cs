7ad42f10ec5f350cf2f940e08a2d908f1123a34f
Kristofer Göransson <kristofer.goransson@telavox.se>
2019-01-06 18:48:57 +0100

    Initial commit

---
+++ src/FasterTooltips/FasterTooltips.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using BattleTech.UI.Tooltips;
+using Newtonsoft.Json;
+using Harmony;
+
+namespace FasterTooltips
+{
+    public class ModSettings
+    {
+        public float TooltipDelay = 0.33f;
+    }
+
+    [HarmonyPatch(typeof(TooltipManager), "SpawnTooltip")]
+    public static class TooltipManager_SpawnTooltip_Patch
+    {
+        static void Prefix(TooltipManager __instance, ref float DelayOverride)
+        {
+            DelayOverride = Mod.settings.TooltipDelay;
+        }
+    }
+
+    public static class Mod
+    {
+        public static ModSettings settings;
+
+        public static void Init(string directory, string settingsJSON)
+        {
+            try
+            {
+                settings = JsonConvert.DeserializeObject<ModSettings>(settingsJSON);
+            }
+            catch (Exception)
+            {
+                settings = new ModSettings();
+            }
+
+            var harmony = HarmonyInstance.Create("com.github.kgoransson.Battletech_FasterTooltips");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
